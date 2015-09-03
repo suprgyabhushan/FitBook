0462a40c06b45889a460106ab9ad128e8481ca9a
suprgyabhushang <Suprgya.56@iiitb.org>
2015-09-03 08:58:39 +0530

    delete

--- ifitfitsifitbits-master/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require_once __DIR__.'/init.php';
-
-    require_once ROUTES_PATH.'/all.php';
-
-    $app->run();
-?>
--- ifitfitsifitbits-master/secrets.sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('GOOGLE_CLIENT', "");
-    define('GOOGLE_SECRET', "");
-
-    define('FITBIT_CLIENT', "");
-    define('FITBIT_SECRET', '');
-?>
